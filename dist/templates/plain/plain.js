5cceaf4f83d57795b804f02f7a2fff61c2bd33be
chrisbarry <chris.barry@makemoredigital.com>
2015-06-21 14:13:08 +0100

    refactoring plain

--- dist/templates/plain/plain.js
+++ dist/templates/plain/plain.js
@@ -1,44 +1,13 @@
-var formfrontPlain = (function ($) {
-    var plain = {};
+var ffTemplatePlain = {};
 
-        var getTemplate = function (filename, callback) {
-
-        var jsFileLocation = $('script[src*=formfront]').attr('src');  // the js file path
-            jsFileLocation = jsFileLocation.replace('plain.js', '');
-            //alert(jsFileLocation);
-
-            $.ajax({
-                url: jsFileLocation + "/templates/plain/" + filename,
-                method: 'GET',
-                //async: false,
-                success: function (data) {
-                    callback(data);
-                }
-            });
-        };
-
-        plain.types = {};
-
-        plain.types.string = {};
-        plain.types.string.type = "string";
-        plain.types.string.templateFile = "field-string.html";
-        plain.types.string.render = function(fieldName, data){
-            var compiled = _.template(plain.types.string.template);
-            return compiled({field:fieldName, data:data});
-        };
-
-
-
-        /*
-        for (var type in plain.types){
-            getTemplate(function(html){
-                types[type].template = html;
-            });
-        }*/
-
-
-
-
-
-    return plain;
-}(jQuery));
+ffTemplatePlain.string = {
+    templateFile: "field-string.html", //needs to turn into .template for real thing
+    generateHtml: function(){
+        var compiled = _.template(templates.stringField);
+        fieldHtml += fieldBody({
+            field: field,
+            data: fields[field],
+            fieldHtml: compiled({field: field})
+        });
+    }
+};
